9d4d85d08c097b755410ed1a8ba18de384d8cd67
simonguozirui <simonguozirui@gmail.com>
2017-01-21 12:23:37 -0500

    created outline of the all four required activities

--- app/src/main/java/joss/jacobo/pennstagram/CompareActivity.java
+++ app/src/main/java/joss/jacobo/pennstagram/CompareActivity.java
@@ -1,8 +1,36 @@
 package joss.jacobo.pennstagram;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 /**
  * Created by simon.guo on 2017-01-21.
  */
 
-public class CompareActivity {
+public class CompareActivity extends AppCompatActivity {
+
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    private static final String STATE_PICTURE_TAKEN_BITMAP = "state_picture_taken_bitmap";
+    private static final String STATE_PICTURE_URI = "state_picture_uri";
+    private static final String IMAGE_NAME = "captured_image";
+
+    private Uri pictureFileUri;
+    private Bitmap pictureTakenBitmap;
+
+    private TextView pictureText;
+    private ImageView platyImageView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crop);
+        pictureText = (TextView) findViewById(R.id.text);
+        platyImageView = (ImageView) findViewById(R.id.platy);
+
+    }
 }
--- app/src/main/java/joss/jacobo/pennstagram/MainActivity.java
+++ app/src/main/java/joss/jacobo/pennstagram/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -175,6 +176,7 @@ public class MainActivity extends AppCompatActivity {
         bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
         byte[] imageBytes = baos.toByteArray();
         String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        Log.i("myTag", encodedImage);
         return encodedImage;
     }
 
