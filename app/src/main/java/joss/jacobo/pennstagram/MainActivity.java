7fc84e53948a9809b40d65ad3d8c8977531cc843
Menelaos Kotsollaris <menelaos@mkotsollaris.com>
2017-01-21 21:09:56 -0500

    New Version with updated design. POST doesn't work

--- app/src/main/java/joss/jacobo/pennstagram/MainActivity.java
+++ app/src/main/java/joss/jacobo/pennstagram/MainActivity.java
@@ -1,6 +1,5 @@
 package joss.jacobo.pennstagram;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -9,14 +8,13 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,8 +29,13 @@ import com.android.volley.toolbox.Volley;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
     private static final String STATE_PICTURE_TAKEN_BITMAP = "state_picture_taken_bitmap";
@@ -42,119 +45,168 @@ public class MainActivity extends AppCompatActivity {
     private Uri pictureFileUri;
     private Bitmap pictureTakenBitmap;
 
-    private TextView pictureText;
     private ImageView platyImageView;
+    private int counter = 0;
 
-    private Button cambutton;
-    private Button searchbutton;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        cambutton = (Button) findViewById(R.id.cam_button);
-        searchbutton = (Button) findViewById(R.id.search_button);
-        setSupportActionBar(toolbar);
+        final ImageButton imageButton1 = (ImageButton) findViewById(R.id.imageButton1);
+        final ImageButton imageButton2 = (ImageButton) findViewById(R.id.imageButton2);
+        final ImageButton imageButton3 = (ImageButton) findViewById(R.id.imageButton3);
+        final ImageButton imageButton4 = (ImageButton) findViewById(R.id.imageButton4);
+        final ImageButton requestButton = (ImageButton) findViewById(R.id.searchButton);
 
-        pictureText = (TextView) findViewById(R.id.text);
-        platyImageView = (ImageView) findViewById(R.id.platy);
 
-        cambutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startImageCapture();
-            }
-
-        });
-
-        searchbutton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        // Set ImageView's onClickListener
-        platyImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        imageButton1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override public void onClick(View view)
+            {
                 startImageCapture();
             }
         });
+
+        imageButton2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override public void onClick(View view)
+            {
+                startImageCapture();
+            }
+        });
+
+        imageButton3.setOnClickListener(new View.OnClickListener()
+        {
+            @Override public void onClick(View view)
+            {
+                startImageCapture();
+            }
+        });
+
+        imageButton4.setOnClickListener(new View.OnClickListener()
+        {
+            @Override public void onClick(View view)
+            {
+                startImageCapture();
+            }
+        });
+
+        requestButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override public void onClick(View v)
+            {
+                HttpPOSTRequestWithParameters();
+            }
+        });
+
 
         // If the savedInstanceState bundle is not null, it means we saved data in it.
         // Grab the data out and restore the state (i.e. Text, Image, and Button Icon)
-        if (savedInstanceState != null) {
+        if(savedInstanceState != null)
+        {
             pictureTakenBitmap = savedInstanceState.getParcelable(STATE_PICTURE_TAKEN_BITMAP);
             pictureFileUri = savedInstanceState.getParcelable(STATE_PICTURE_URI);
-            setContent();
         }
 
+    }
+
+    private void different() throws IOException
+    {
+        HttpURLConnection httpURLConnection;
+        URL url = new URL("http://45.33.95.66:8081/?image=23232.jpg");
+        httpURLConnection = (HttpURLConnection) url.openConnection();
+        httpURLConnection.connect();
     }
 
     /**
      * Set the screen's content depending on whether the user has already taken a picture or not.
      */
-    private void setContent() {
-        if (pictureTakenBitmap != null) {
-            //pictureText.setText(R.string.retake_picture);
-            platyImageView.setImageBitmap(pictureTakenBitmap);
-
-        } else {
-            //pictureText.setText(R.string.take_picture);
-            platyImageView.setImageResource(R.drawable.platy);
-
+    private void setContent(Bitmap b)
+    {
+        counter++;
+        final ImageButton imageButton1 = (ImageButton) findViewById(R.id.imageButton1);
+        final ImageButton imageButton2 = (ImageButton) findViewById(R.id.imageButton2);
+        final ImageButton imageButton3 = (ImageButton) findViewById(R.id.imageButton3);
+        final ImageButton imageButton4 = (ImageButton) findViewById(R.id.imageButton4);
+        if(counter == 1)
+        {
+            System.out.println("1");
+            System.out.println(b);
+            imageButton1.setImageBitmap(b);
         }
-
+        else if(counter == 2)
+        {
+            System.out.println("2");
+            imageButton2.setImageBitmap(b);
+        }
+        else if(counter == 3)
+        {
+            System.out.println("3");
+            imageButton3.setImageBitmap(b);
+        }
+        else if(counter == 4)
+        {
+            System.out.println("4");
+            imageButton4.setImageBitmap(b);
+        }
     }
 
-    private void startImageCapture() {
+    private void startImageCapture()
+    {
         // create Intent to take a picture and return control to the calling application
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
         pictureFileUri = getOutputMediaFileUri(this, IMAGE_NAME); // create a file to save the image
-        if (pictureFileUri != null) {
+        if(pictureFileUri != null)
+        {
             intent.putExtra(MediaStore.EXTRA_OUTPUT, pictureFileUri); // set the image file name
-
             // start the image capture Intent
             startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-        } else {
-            Toast.makeText(this, "There was an error creating the file to save the image to", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this, "There was an error creating the file to save the image to",
+                           Toast.LENGTH_SHORT).show();
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    @Override protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
+        if(requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE)
+        {
+            if(resultCode == RESULT_OK)
+            {
                 // Image captured and saved to pictureFileUri specified in the Intent
                 File image = new File(pictureFileUri.getPath());
-                if (image.exists()) {
+                if(image.exists())
+                {
                     pictureTakenBitmap = BitmapFactory.decodeFile(image.getAbsolutePath());
-                    setContent();
-
-                    uploadImage(getStringImage(pictureTakenBitmap));
+                    getStringImage(pictureTakenBitmap);
+                    setContent(pictureTakenBitmap);
                 }
-            } else if (resultCode == RESULT_CANCELED) {
+            }
+            else if(resultCode == RESULT_CANCELED)
+            {
                 // User cancelled the image capture
                 // Image captured and saved to pictureFileUri specified in the Intent
-            } else {
+            }
+            else
+            {
                 // Image capture failed, advise user
             }
         }
     }
 
 
-    @Nullable
-    private Uri getOutputMediaFileUri(Context context, String name) {
+    @Nullable private Uri getOutputMediaFileUri(Context context, String name)
+    {
         // Get external storage directory
         File filesDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         // Get or Create image folder
         File imagesDir = new File(filesDir, "images");
-        if (imagesDir.mkdir() || imagesDir.isDirectory()) {
+        if(imagesDir.mkdir() || imagesDir.isDirectory())
+        {
             // If images folder exists create an image file to save our picture taken
             // and return it's Uri.
             return Uri.fromFile(new File(imagesDir, name.replace("/", "_") + ".jpeg"));
@@ -162,8 +214,8 @@ public class MainActivity extends AppCompatActivity {
         return null;
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
+    @Override protected void onSaveInstanceState(Bundle outState)
+    {
         super.onSaveInstanceState(outState);
         // Save image bitmap and it's uri when activity gets destroyed
         // (i.e. on screen rotation)
@@ -171,7 +223,8 @@ public class MainActivity extends AppCompatActivity {
         outState.putParcelable(STATE_PICTURE_URI, pictureFileUri);
     }
 
-    public String getStringImage(Bitmap bmp) {
+    public String getStringImage(Bitmap bmp)
+    {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
         byte[] imageBytes = baos.toByteArray();
@@ -181,7 +234,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void uploadImage(String imageString) {
+    public void uploadImage(String imageString)
+    {
         final TextView mTextView = (TextView) findViewById(R.id.text);
 
         // Instantiate the RequestQueue.
@@ -189,24 +243,84 @@ public class MainActivity extends AppCompatActivity {
         String url = "http://45.33.95.66:8081/?image=" + imageString;
 
         // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
+        StringRequest stringRequest =
+                new StringRequest(Request.Method.POST, url, new Response.Listener<String>()
+                {
+                    @Override public void onResponse(String response)
+                    {
                         // Display the first 500 characters of the response string.
                         mTextView.setText("Response is: " + response.substring(0, 500));
                     }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                //mTextView.setText("That didn't work!");
-            }
-        });
+                }, new Response.ErrorListener()
+                {
+                    @Override public void onErrorResponse(VolleyError error)
+                    {
+                        //mTextView.setText("That didn't work!");
+                    }
+                });
         // Add the request to the RequestQueue.
         queue.add(stringRequest);
-
     }
 
+    public void HttpPOSTRequestWithParameters()
+    {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "45.33.95.66:8081";
+        StringRequest postRequest =
+                new StringRequest(Request.Method.POST, url, new Response.Listener<String>()
+                {
+                    @Override public void onResponse(String response)
+                    {
+                        Log.d("Response", response);
+                    }
+                }, new Response.ErrorListener()
+                {
+                    @Override public void onErrorResponse(VolleyError error)
+                    {
+                        Log.d("ERROR", "error => " + error.toString());
+                    }
+                })
+                {
+                    // this is the relevant method
+                    @Override protected Map<String, String> getParams()
+                    {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("image", "hey gustav");
+                        return params;
+                    }
+                };
+        queue.add(postRequest);
+    }
 
+    public void put()
+    {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "45.33.95.66:8081";
+        StringRequest putRequest =
+                new StringRequest(Request.Method.PUT, url, new Response.Listener<String>()
+                {
+                    @Override public void onResponse(String response)
+                    {
+                        // response
+                        Log.d("Response", response);
+                    }
+                }, new Response.ErrorListener()
+                {
+                    @Override public void onErrorResponse(VolleyError error)
+                    {
+                        // error
+                        Log.d("Error.Response", error.getMessage());
+                    }
+                })
+                {
+                    @Override protected Map<String, String> getParams()
+                    {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("image", "Alif");
+                        return params;
+                    }
+                };
+        queue.add(putRequest);
+    }
 }
 
